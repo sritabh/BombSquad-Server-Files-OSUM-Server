8a63ceee58caec033aa6093c29d1b397ece13321
SobyDamn <43126956+SobyDamn@users.noreply.github.com>
2020-01-03 18:30:40 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# place any of your own overrides here225944
+# see bombsquad_server for details on what you can override
+# examples (uncomment to use):
+config['partyName'] = u'\ue043Teams #OSUM\ue043'
+config['sessionType'] = 'teams'
+config['maxPartySize'] = 9
+config['port'] = 16511
+config['playlistCode'] = 228323
+config['statsURL'] = 'http://osum.ml/#${ACCOUNT}'
+config['enableTelnet'] = True
+config['telnetPassword'] = '@69'
+config['telnetPort'] = 43211
